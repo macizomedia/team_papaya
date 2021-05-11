bba6313e5315843308520d19e2dc5057ac4e9107
Luiza <luizafcampos@gmail.com>
2021-05-11 12:00:30 +0200

    footer

--- src/componets/footer/index.jsx
+++ src/componets/footer/index.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
-    return (
-        <div>
-            //TODO Navigation
-        </div>
-    )
+    return <div>©️2021 Blas, Luiza, Viola</div>;
 }

c8acc0c390766a3873f6baef4e353666b09a49de
macizomedia <info@macizomedia.com>
2021-05-18 13:01:50 +0200

    commented admin component

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -6,30 +6,34 @@ import { switchMap, map, take, takeWhile, filter, flatMap } from "rxjs/operators
 
 let URL = 'https://restcountries.eu/rest/v2/all';
 
+/* Main Fetch Observable from api folder */
 const data$ = fetchData(URL);
 
+/* Definition of type of data to Subscribe */
 const currencies$ = data$.pipe(
-  map(data => data.map(data => data.currencies)),
-  map(units => units.flat(2)),
-  map(currency => currency.map(unit => unit.name))
+  map(data => data.map(data => data.currencies)), /* data to data Array */
+  map(units => units.flat(2)), /* Flat Array */
+  map(currency => currency.map(unit => unit.name)) /* Map over Array of Objects */
 )
 
+/* Definition of type of data with switchMap (fetching image from other source) */
 const photos$ = data$.pipe(
-  map(data => data.map(data => data.name.toLowerCase())),
-  switchMap(val => fetchImages(val)),
-  map(result => result.map(photo => photo.urls))
+  map(data => data.map(data => data.name.toLowerCase())), /* getting data name to goes to switchMap */
+  switchMap(val => fetchImages(val)), /* val is country name in lowerCase */
+  map(result => result.map(photo => photo.urls)) /* Result is nested look on fetchImages function to see what is result */
 )
 
+/* Same as above but getting the entire Photo Object */
 const photosArr$ = data$.pipe(
   map(data => data.map(data => data.name.toLowerCase())),
   switchMap(val => fetchImages(val)),
-  map(result => result.flat(2))
+  map(result => result.flat(2)) /* Flat the result to get an Array of Objects */
 )
 
-photosArr$.subscribe(console.log)
-
+photosArr$.subscribe(console.log) /* we subscribe here and pass a function to consume the data */
 
 /* CUSTOM HOOK */
+/* This hook is called inside the component with and observable and a setState function */
 const useObservable = (observable, setFunc) => {
   useEffect(() => {
     let subscription = observable.subscribe(result => {
@@ -46,8 +50,9 @@ const Home = ({ history }) => {
   const [currencies, setCurrencies] = useState([])
   const [photos, setPhotos] = useState()
 
+  /* Using Custom Hooks to consume data a feed the state */
   useObservable(photosArr$, setPhotos)
-  useObservable(currencies$, setCurrencies)
+  useObservable(currencies$, setCurrencies) 
 
   return (
     <>
@@ -61,6 +66,7 @@ const Home = ({ history }) => {
       ) : (null)}
       <Router history={history}>
         <p>Currently logged as {currentUser.user}</p>
+        {/* USE pre tag and JSON.Stringify to Understand data structure */}
         <pre>{JSON.stringify(currencies, null, 4)}</pre>
       </Router>
     </>

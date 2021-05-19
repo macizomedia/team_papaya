934c0a31c4d9e1172e101853bbe69478a7b0ea74
Luiza <luizafcampos@gmail.com>
2021-05-19 20:01:22 +0200

    ...

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,92 +1,81 @@
-
 import React, { useState } from "react";
 import { VectorMap } from "react-jvectormap";
 
-
-const randomColor = require('randomcolor'); // import the script
+const randomColor = require("randomcolor"); // import the script
 
 randomColor({
-  count: 10,
-  // hue: 'green',
-  luminosity: 'light'
+    count: 10,
+    // hue: 'green',
+    luminosity: "light",
 });
 
-const mapData = {
-
-
-
-};
+const mapData = {};
 
 // let newColor;
 let color;
 
 const handleClick = (e, countryCode) => {
-
-
-  color = randomColor();
-  console.log(color); 
-  // newColor = await Color();
-  // console.log(countryCode);
-  // console.log(newColor);
+    color = randomColor();
+    console.log(color);
+    // newColor = await Color();
+    // console.log(countryCode);
+    // console.log(newColor);
 };
-
-
-
 
 const Map = () => {
+    const [state, setState] = useState();
 
-  const [state, setState] = useState();
-
-  return (
-    <div className="map ">
-      <h2 className="subtitle">Countries you have visited</h2>
-      <p> <i className="subtitle">(click on the map to select)</i> </p>
-      <VectorMap
-        map={"world_mill"}
-        backgroundColor="#95c8f5" //change it to ocean blue: #0077be
-        zoomOnScroll={true}
-        containerStyle={{
-          width: "100%",
-          height: "520px"
-        }}
-        onRegionClick={handleClick} //gets the country code
-        containerClassName="map"
-        regionStyle={{
-          initial: {
-            fill: "#5e6061",
-            "fill-opacity": 3,
-            stroke: " none",
-            "stroke-width": 0,
-            "stroke-opacity": 0
-          },
-          hover: {
-            fill: "#6E8C03",
-            "fill-opacity": 2,
-            cursor: "pointer"
-          },
-          selected: {
-            fill: {color}
-          },
-          selectedHover: {
-            fill: " #A9BF7A",
-            "fill-opacity": 2,
-          }
-          
-        }}
-        regionsSelectable={true}
-        series={{
-          regions: [
-            {
-              values: mapData, //this is your data
-              scale: ["#BFCDD9", "#BF8756", "#8C4F2B"], //your color game's here
-              normalizeFunction: "polynomial",
-              fill: " #D9946C"
-            }
-          ]
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="map ">
+            <h2 className="subtitle">Countries you have visited</h2>
+            <p>
+                {" "}
+                <i className="subtitle">(click on the map to select)</i>{" "}
+            </p>
+            <VectorMap
+                map={"world_mill"}
+                backgroundColor="#95c8f5" //change it to ocean blue: #0077be
+                zoomOnScroll={true}
+                containerStyle={{
+                    width: "100%",
+                    height: "520px",
+                }}
+                onRegionClick={handleClick} //gets the country code
+                containerClassName="map"
+                regionStyle={{
+                    initial: {
+                        fill: "#5e6061",
+                        "fill-opacity": 3,
+                        stroke: " none",
+                        "stroke-width": 0,
+                        "stroke-opacity": 0,
+                    },
+                    hover: {
+                        fill: "#6E8C03",
+                        "fill-opacity": 2,
+                        cursor: "pointer",
+                    },
+                    selected: {
+                        fill: { color },
+                    },
+                    selectedHover: {
+                        fill: " #A9BF7A",
+                        "fill-opacity": 2,
+                    },
+                }}
+                regionsSelectable={true}
+                series={{
+                    regions: [
+                        {
+                            values: mapData, //this is your data
+                            scale: ["#BFCDD9", "#BF8756", "#8C4F2B"], //your color game's here
+                            normalizeFunction: "polynomial",
+                            fill: " #D9946C",
+                        },
+                    ],
+                }}
+            />
+        </div>
+    );
 };
 export default Map;
-
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,54 +1,81 @@
 import React from "react";
 import { logout, useAuthState, useAuthDispatch } from "../../store/index";
-import papaya from "../../img/papaya_Logo.png";
 
 export default function index({ history }) {
-
-  const { currentUser } = useAuthState();
-  const dispatch = useAuthDispatch();
-  const handleLogout = () => {
-    console.log("LOGIN-OUT");
-    logout(dispatch);
-    history.push("/");
-  };
-  return (
-    <div className="header header-fixed unselectable header-animated">
-      <div className="header-brand">
-        <div className="nav-item no-hover">
-          <a href="/">
-
-            <h6 className="title">{currentUser.user !== "" ? currentUser.user : "Papaya"}</h6>
-          </a>
-        </div>
-        <div className="nav-item nav-btn" id="header-btn">
-          <span></span> <span></span> <span></span>
-        </div>
-      </div>
-      <div className="header-nav" id="header-menu">
-        <div className="nav-left">
-          <div className="nav-item text-center">
-            <a href="https://github.com/macizomedia/team_papaya">
-              <span className="icon">
-                <i
-                  className="fas fa-wrapper fa-code-branch"
-                  aria-hidden="true"
-                ></i>
-              </span>
-            </a>
-          </div>
-        </div>
-        <div className="nav-right">
-          <div className="nav-item has-sub toggle-hover" id="dropdown">
-            {!currentUser.token ? (<a href="/login"><i class="fas fa-sign-in-alt"></i></a>) : (<a href="#dropdown" className="nav-dropdown-link">Menu</a>)}
-            <ul className="dropdown-menu dropdown-animated" role="menu">
-              <li>
-                {!currentUser.token ? (<a href="/signup">SignUp</a>) : (<a onClick={handleLogout} href="/">Logout</a>)}
-              </li>
-              <li>
-                {!currentUser.token ? (<a href="/about">About</a>) : (<a href="/dashboard">My Dashboard</a>)}
-              </li>
-            </ul>
-          </div>
+    const { currentUser } = useAuthState();
+    const dispatch = useAuthDispatch();
+    const handleLogout = () => {
+        console.log("LOGIN-OUT");
+        logout(dispatch);
+        history.push("/");
+    };
+    return (
+        <div className="header header-fixed unselectable header-animated">
+            <div className="header-brand">
+                <div className="nav-item no-hover">
+                    <a href="/">
+                        <h6 className="title">
+                            {currentUser.user !== ""
+                                ? currentUser.user
+                                : "Papaya"}
+                        </h6>
+                    </a>
+                </div>
+                <div className="nav-item nav-btn" id="header-btn">
+                    <span></span> <span></span> <span></span>
+                </div>
+            </div>
+            <div className="header-nav" id="header-menu">
+                <div className="nav-left">
+                    <div className="nav-item text-center">
+                        <a href="https://github.com/macizomedia/team_papaya">
+                            <span className="icon">
+                                <i
+                                    className="fas fa-wrapper fa-code-branch"
+                                    aria-hidden="true"
+                                ></i>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div className="nav-right">
+                    <div
+                        className="nav-item has-sub toggle-hover"
+                        id="dropdown"
+                    >
+                        {!currentUser.token ? (
+                            <a href="/login">
+                                <i class="fas fa-sign-in-alt"></i>
+                            </a>
+                        ) : (
+                            <a href="#dropdown" className="nav-dropdown-link">
+                                Menu
+                            </a>
+                        )}
+                        <ul
+                            className="dropdown-menu dropdown-animated"
+                            role="menu"
+                        >
+                            <li>
+                                {!currentUser.token ? (
+                                    <a href="/signup">SignUp</a>
+                                ) : (
+                                    <a onClick={handleLogout} href="/">
+                                        Logout
+                                    </a>
+                                )}
+                            </li>
+                            <li>
+                                {!currentUser.token ? (
+                                    <a href="/about">About</a>
+                                ) : (
+                                    <a href="/dashboard">My Dashboard</a>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
